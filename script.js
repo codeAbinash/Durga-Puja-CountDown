c107a7e26d125530da9f8602710c76d75dc806cb
Abinash Karmakar <codeAbinash@gmail.com>
2022-09-25 12:55:08 +0530

    Removed defaut wish name

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // var fromToTxt = "is wishing you";
 // if(perm.length>0){
-    //     perm = perm.split("&");
-    //     fromToTxt = `Hello ${perm[1]}!!!<br> ${perm[0]} is wishing you`;
-    //     localStorage.durgaPujaYou = perm[1];
-    // }
-    // else
-    //     alert("<0");
+//     perm = perm.split("&");
+//     fromToTxt = `Hello ${perm[1]}!!!<br> ${perm[0]} is wishing you`;
+//     localStorage.durgaPujaYou = perm[1];
+// }
+// else
+//     alert("<0");
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     localStorage.durgaPujaYou = "";
-    document.getElementById("dhak").volume=0.2;
+    document.getElementById("dhak").volume = 0.2;
     var perm = window.location.search.substr(1);
-    var fromToTxt = "Dark Rose Software is wishing you";
-    if(perm.length>0){
+    var fromToTxt = "... is wishing you";
+    if (perm.length > 0) {
         perm = perm.split("-");
         // Remove Extra space from the names
-        perm = [perm[0].trim(),perm[1].trim()];
+        perm = [perm[0].trim(), perm[1].trim()];
         fromToTxt = `Hello ${perm[0]} !!!<br> ${perm[1]} is wishing you`;
         localStorage.durgaPujaYou = perm[0];
-    }else{
+    } else {
         //alert("nothing");
     }
     window.title = "Happy Durga puja in Advance from " + perm[1];
-    
+
     document.getElementById("fromTo").innerHTML = fromToTxt;
     document.getElementById("yourName").value = localStorage.durgaPujaYou;
 
@@ -32,33 +32,33 @@ window.addEventListener("load",()=>{
 var you = localStorage.durgaPujaYou;
 var friend = "";
 var link = "";
-function setName(n){
+function setName(n) {
     you = n;
     makeLink();
 }
-function setFriendName(n){
+function setFriendName(n) {
     friend = n;
     makeLink();
 }
-function makeLink(){
-    var host = window.location.href.substr(0,window.location.href.indexOf("?"));
-    if(host.length==0)
-        host=window.location.href;
-    link =host + "?"+friend.trim()+"-"+you.trim();
+function makeLink() {
+    var host = window.location.href.substr(0, window.location.href.indexOf("?"));
+    if (host.length == 0)
+        host = window.location.href;
+    link = host + "?" + friend.trim() + "-" + you.trim();
     document.getElementById("share").innerHTML = `Share Link to ${friend} <br><br>Click to Copy Link`;
     document.getElementById("hiddenText").value = link;
-    if(friend.length>0&&you.length>0){
-        document.getElementById("shareWhatsapp").href ="whatsapp://send?text=" + link;
+    if (friend.length > 0 && you.length > 0) {
+        document.getElementById("shareWhatsapp").href = "whatsapp://send?text=" + link;
     }
 }
-function copyLink(){
-    if(friend.length>0&&you.length>0){
+function copyLink() {
+    if (friend.length > 0 && you.length > 0) {
         var copyText = document.getElementById("hiddenText");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
         alert("Link Copied, now paste it to " + friend);
-    }else{
+    } else {
         alert("Enter Your Friend's name & your name")
     }
 
@@ -68,10 +68,10 @@ function copyLink(){
 var now = new Date();
 var dt = new Date("2 October 2022");
 var gap = dt.getTime() - now.getTime();
-var day = Math.round(gap/(1000*60*60*24)) + 1;
+var day = Math.round(gap / (1000 * 60 * 60 * 24)) + 1;
 var numList = "০১২৩৪৫৬৭৮৯";
 day = day.toString();
 var numStr = "";
-for(var i=0;i<day.length;i++)
-    numStr+=numList[Number(day[i])];
-document.getElementById("left").innerHTML="আর " + numStr +" দিন"
+for (var i = 0; i < day.length; i++)
+    numStr += numList[Number(day[i])];
+document.getElementById("left").innerHTML = "আর " + numStr + " দিন"
